336223024feb190819aedc52bf077bc1dc55b9ec
Wesley Yakubu Ande <yakubuwesley@gmail.com>
2023-03-20 13:59:31 -0700

    0x00 hello, world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Entry poit
+*Return : always 0 (sucess)
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcomeis a piece of art,\n");
+	return (0);
+}
