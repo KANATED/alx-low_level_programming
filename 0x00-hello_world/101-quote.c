22b9acc23fc6f80f23a793972bafa45cf71dc5e2
Wesley Yakubu Ande <yakubuwesley@gmail.com>
2023-03-20 19:30:11 -0700

    hello world 101i

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Ac program that prints a line to the standard error
+ * main - A program that prints a line to the standard error
  * Return: 1 (Sucess)
  */
 int main(void)
 {
-fput("and that peice of art is useful\" -Dora Korpar,2015-10-18\n", stdout);
+write(2, "and that peice of art is useful\" -Dora Korpar, 2015-10-18\n", stdout);
 return (1);
 }
