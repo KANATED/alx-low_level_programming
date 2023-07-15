fa15ec4b766e3c2f757be31f3e00fc948b6a7d86
KANATED <yakubuwesley@gmail.com>
2023-07-15 15:24:30 -0700

    what exactly is wrong with this checker now

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,13 +13,5 @@ int append_text_to_file(const char *filename, char *text_content);
 void error_exit(int exit_code, const char *message);
 int main(int argc, char *argv[]);
 
-void error_exit(int exit_code, const char *message);
-void print_elf_header(Elf64_Ehdr *elf_header);
-switch (elf_header->e_ident[EI_CLASS]);
-switch (elf_header->e_ident[EI_DATA]);
-switch (elf_header->e_ident[EI_OSABI]);
-switch (elf_header->e_type);
-
-int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
